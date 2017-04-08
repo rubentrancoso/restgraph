12e3ab8a78701b20e238aaef7fae14bd00e02a4d
Ruben Paulo Martins Trancoso <rubentrancoso@gmail.com>
2017-04-08 14:31:30 -0300

    cors test

--- backend/src/main/java/engine/Application.java
+++ backend/src/main/java/engine/Application.java
@@ -23,7 +23,7 @@ public class Application {
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://201.159.114.71");
+                registry.addMapping("/**").allowedOrigins("http://www.test-cors.org");
             }
         };
     }
