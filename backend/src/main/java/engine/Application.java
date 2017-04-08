74680e2404be326bf8619b5493e261c017ab62de
Ruben Paulo Martins Trancoso <rubentrancoso@gmail.com>
2017-04-08 13:48:49 -0300

    cors origin

--- backend/src/main/java/engine/Application.java
+++ backend/src/main/java/engine/Application.java
@@ -23,7 +23,7 @@ public class Application {
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**");
+                registry.addMapping("/**").allowedOrigins("http://201.159.114.71");
             }
         };
     }
