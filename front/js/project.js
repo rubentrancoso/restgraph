ae4c1bf10f402357f19a23a552b75a42eb7c0ceb
Ruben Paulo Martins Trancoso <rubentrancoso@gmail.com>
2017-04-08 21:40:22 -0300

    front

---
+++ front/js/project.js
@@ -0,0 +1,29 @@
+angular.module('project', ['ngRoute'])
+ 
+.service('Projects', function($http) {
+  
+})
+ 
+.config(function($routeProvider) {
+ 
+  $routeProvider
+    .when('/', {
+      controller:'ProjectController as projectCtrl',
+      templateUrl:'templates/list.html'
+    })
+    .otherwise({
+      redirectTo:'/'
+    });
+})
+ 
+.controller('ProjectController',
+  function($scope, $http) {
+    var url = "http://vanhackathon.doteva.com:8080/api/hello/folks";
+
+    $http.get(url)
+    .then(function(response) {
+        $scope.content = response.data;
+        $scope.statuscode = response.status;
+        $scope.statustext = response.statusText; 
+    });
+});
