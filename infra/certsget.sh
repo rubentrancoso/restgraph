5cc101f8cfb3dbdc0e3cc57c6d40cf1d6df374d0
Ruben <rubentrancoso@gmail.com>
2017-04-23 23:31:33 +0000

    fix scripts

--- infra/certsget.sh
+++ infra/certsget.sh
@@ -19,7 +19,7 @@ sudo cat <<EOF>  /opt/server/certsrenew.sh
 rm -rf /var/lib/neo4j/certificates/neo4j.*
 cp /etc/letsencrypt/live/$CERTDOMAIN/cert.pem /var/lib/neo4j/certificates/neo4j.cert
 cp /etc/letsencrypt/live/$CERTDOMAIN/privkey.pem /var/lib/neo4j/certificates/neo4j.key
-service nginx restart
+# service nginx restart
 EOF
 
 sudo cat <<EOF>  /etc/cron.daily/certscron.sh
